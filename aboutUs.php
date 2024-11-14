b27c2e9f351dbdd820f4cc9a63ad45f87e6553be
Dinithi Nanayakkara <dinijayo@gmail.com>
2024-11-14 21:12:03 +0530

    added pictures to about page

--- aboutUs.php
+++ aboutUs.php
@@ -24,8 +24,8 @@ require_once 'functions.php';
     <p>To be a place where every sip sparks connection and every visit feels like home. At LatteLane Cafe, we envision a community brought together by the love of quality coffee, warm conversations, and a cozy atmosphere that nurtures creativity, comfort, and joy. We aim to inspire moments of relaxation and meaningful connection, one cup at a time.</p>
 </section>
 
-<img src="../images/visionimagewoman.jpg"  class="circular-image1">
-<img src="../images/coffeecup.png"  class="circular-image2">
+<img src="./src/images/visionimagewoman.jpg"  class="circular-image1">
+<img src="./src/images/teacup.png"  class="circular-image2">
 
 <section class="mission-section">
     <h2>Our Mission</h2>
@@ -33,7 +33,7 @@ require_once 'functions.php';
 </section>
 
 <section class="special">
-<img src="../images/spillingDrink.png"  class="image3">
+<img src="./src/images/spillingDrink.png"  class="image3">
     <h2>Why are We so Special?</h2>
     <p>At LatteLane Cafe, our mission is to create a thriving community centered around the love of coffee. We believe in the power of coffee to bring people together, inspire connections, and enrich lives.</p>
     <button class='learnbtn' onclick='window.location.href=`index.php`'>Learn More</button> 
